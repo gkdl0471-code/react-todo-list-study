8b253f01d11d7aca2ed0dab6406966c5ec140909
soyeon <gkdl0471@gmail.com>
2025-11-22 15:54:30 +0900

    4. 날씨 표시 기능 (Open API 활용)

--- components/Clock.jsx
+++ components/Clock.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 
 export function Clock () {
   const [time, setTime] = useState(new Date());
+  const [weather, setWeather] = useState(null);
+  const API_KEY = "0f054b4501d279ac9453ec8a1b80ed7b";
 
   useEffect(() => {
   const timer = 
@@ -9,6 +11,19 @@ export function Clock () {
       setTime(new Date());
     }, 1000);
   return () => clearInterval(timer);
+  },[]);
+
+  useEffect(() => {
+    async function fetchWeather() {
+      try {
+        const res = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=Incheon&appid=${API_KEY}&units=metric&lang=kr`);
+        const data = await res.json();
+        setWeather(data);
+      } catch (err) {
+        console.error("날씨 불러오기 실패:", err);
+      }
+    }
+    fetchWeather();
   },[]);
 
   const year = time.getFullYear();
@@ -24,13 +39,20 @@ export function Clock () {
     hour12: false
   });
 
+  const weatherline = weather ? (<div className="weather-box">
+          <div className="weather-temp">{weather.main.temp}°C, {weather.weather[0].description}</div>
+        </div>)
+      : (<div className="weather-loading">날씨 불러오는 중...</div>)
+
   return (
     <>
     <div className='dashboard-clock'>
-      <div className='clock-label'>📅{formattedDate}</div>
+      <div className='clock-label'>📅 {formattedDate}</div>
+      <div className='weather-temp'>🌏 WEATHER : {weatherline}</div>
       <div className='clock-time'>{formattedTime}</div>
     </div>
+  
     </>
-  )
+  );
 }
 
